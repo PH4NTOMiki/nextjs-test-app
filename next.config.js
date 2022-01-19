3e725b5551ba9109eeb836d9f1383ea104ab9595
Mihael <mihaelmiki123@live.com>
2022-01-19 13:18:26 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,41 +1,34 @@
+const { walkSync, parse, exec, devalue } = require('./config-helpers');
 const webpack = require('webpack');
-const fs = require('fs');
-const path = require('path');
-const walkSync = (dir, filelist = []) => {
-	fs.readdirSync(dir).forEach(file => {
-		filelist = fs.statSync(path.join(dir, file)).isDirectory()
-		? walkSync(path.join(dir, file), filelist)
-		: filelist.concat(path.join(dir, file));
-	});
-	return filelist;
-}
 
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  reactStrictMode: true,
-  outputFileTracing: false,
-  "webpack": (config, { isServer, dev }) => {
-	if(!isServer){
-	  config.plugins.push(new webpack.IgnorePlugin({
-		resourceRegExp: /api\//
-	  }));
-	  config.plugins.push(new webpack.IgnorePlugin({
-		resourceRegExp: /^https?$/
-	  }));
-	  config.plugins.push(new webpack.IgnorePlugin({
-		resourceRegExp: /lib\/_fetch_server/
-	  }));
-	} else {
-	  const API_LIST = JSON.stringify(walkSync('./pages/api').filter(f => f.endsWith('.js')).map(f => f.replace('pages/api/','').replace('.js', '')));
-	  console.log({API_LIST});
-	  config.plugins.push(new webpack.DefinePlugin({
-		'process.env.API_LIST': API_LIST
-	  }));
+	reactStrictMode: true,
+	outputFileTracing: false,
+	"webpack": (config, { isServer, dev }) => {
+		if(!isServer){
+			config.plugins.push(new webpack.IgnorePlugin({
+				resourceRegExp: /api\//
+			}));
+			config.plugins.push(new webpack.IgnorePlugin({
+				resourceRegExp: /^https?$/
+			}));
+			config.plugins.push(new webpack.IgnorePlugin({
+				resourceRegExp: /lib\/_fetch_server/
+			}));
+		} else {
+			const apiMap = {};
+			const apiList = walkSync('./pages/api').filter(f => f.endsWith('.js')).map(f => f.replace('pages/api','').replace('.js', '')).reverse().map(e => {return (e.includes('[')&&e.includes(']')&&(apiMap[e] = parse(e))), e;});
+			console.log({apiMap:devalue(apiMap), apiList})
+			config.plugins.push(new webpack.DefinePlugin({
+				'process.env.API_LIST': JSON.stringify(apiList),
+				'process.env.API_LIST_JSON': devalue(apiMap)
+			}));
+		}
+		return config;
 	}
-	return config;
-  }
 };
 
 module.exports = nextConfig;
---
+++ pages/_middleware.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @type {import('next/server').NextMiddleware}
+ */
+export function middleware(req, ev) {
+	console.log(req);
+	//return new Response('Hello, world!')
+}
